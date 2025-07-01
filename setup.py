909653668d8bfa4515515e4474275bc8d8a15c45
astro-jingtao <634790985@qq.com>
2025-07-02 01:15:50 +0800

    add pyproject

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
       name='poissonpy',
-      version='1.0.0',
+      version='1.0.1',
       description='Plug-and-play 2D Poisson equations library.',
       long_description="Plug-and-play 2D Poisson equation library. Useful in scientific computing, image and video processing, computer graphics.",
-      url='https://github.com/bchao1/poissonpy',
+      url='https://github.com/astro-jingtao/poissonpy',
       keywords = "pde poisson-equation laplace-equation scientific-computing poisson",
-      author='bchao1',
+      author='bchao1, Tao Jing',
       license='MIT',
       packages=find_packages(),
       python_requires=">=3.6"
